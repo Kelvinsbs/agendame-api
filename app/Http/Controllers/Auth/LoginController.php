8dbbc14276966f7e0311d8fd3df6dc3d8e7f54bd
Kelvinsbs <nivlek.barcelos@gmail.com>
2025-06-11 13:09:41 -0300

    validacao de login pt1

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,17 +3,16 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function __invoke() {
-        $login = [
-            'email' => 'test@example.com',
-            'password' => 'password',
-        ];
+    public function __invoke(LoginRequest $request) {
 
-        if (auth()->attempt($login)) {
+        $input = $request->validated();
+
+        if (auth()->attempt($input)) {
             request()->session()->regenerate();
 
             return auth()->user();
