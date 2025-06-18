3b05fe345b70e40c76c15b1d11d2b9d3bad19c5a
Kelvinsbs <nivlek.barcelos@gmail.com>
2025-06-18 12:58:52 -0300

    exception no login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,18 +5,19 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Resources\UserResource;
-use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function __invoke(LoginRequest $request) {
+    public function __invoke(LoginRequest $request): UserResource {
 
         $input = $request->validated();
 
-        if (auth()->attempt($input)) {
-            request()->session()->regenerate();
-
-            return new UserResource(auth()->user());
+        if (!auth()->attempt($input)) {
+            throw new \App\Exceptions\InvalidAuthenticationException();
         }
+
+        request()->session()->regenerate();
+
+        return new UserResource(auth()->user());
     }
 }
