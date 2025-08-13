118e18b209bf27ad89acc986755dfc0cfb966f78
Kelvinsbs <nivlek.barcelos@gmail.com>
2025-08-13 13:17:39 -0300

    envio de email com mailtrap.io

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\User\MeController;
+use App\Mail\WelcomeMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', LoginController::class);
@@ -11,4 +13,8 @@ Route::post('register', RegisterController::class);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('me', [MeController::class, 'show']);
+});
+
+Route::post('email', function() {
+    Mail::to('test@test.com')->send(new WelcomeMail());
 });
