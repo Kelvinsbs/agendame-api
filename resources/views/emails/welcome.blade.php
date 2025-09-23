d0d6b2bb4d0a0ca8dfc4baf744f40f5b0fb82935
Kelvinsbs <nivlek.barcelos@gmail.com>
2025-09-23 18:58:03 -0300

    template de email

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,1 +1,24 @@
-Welcome! {{ $user->first_name }}
+@extends('emails.layouts.default')
+
+@section('content')
+    <p>Olá {{ $user->first_name }}, </p>
+    <p>Seja bem vindo ao {{ config('app.name') }} </p>
+    <p>Por favor, clique no botão abaixo para verificar a sua conta.</p>
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+    <tbody>
+        <tr>
+        <td align="left">
+            <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+            <tbody>
+                <tr>
+                <td> <a href="http://htmlemail.io" target="_blank">Confirmar Conta</a> </td>
+                </tr>
+            </tbody>
+            </table>
+        </td>
+        </tr>
+    </tbody>
+    </table>
+    <p>This is a really simple email template. It's sole purpose is to get the recipient to click the button with no distractions.</p>
+    <p>Good luck! Hope it works.</p>
+@endsection
